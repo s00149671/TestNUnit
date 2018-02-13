754e489cd6027103f489f11fee40602e8a49acce
s00149671 <s00149671@mail.itsligo.ie>
2018-02-13 16:01:51 +0000

    Testing

--- TestingAssign1/Class1.cs
+++ TestingAssign1/Class1.cs
@@ -8,5 +8,34 @@ namespace TestingAssign1
 {
     public class Class1
     {
+        public float CalcPremium(int age, string gender)
+        {
+            float premium;
+
+            if (gender == “female”)
+	if ((age > = 18) && (age <= 30))
+                premium = 5.0;
+            else
+        if (age >= 31)
+                premium = 2.50;
+            else
+                premium = 0.0;
+else
+	if (gender == “male”)
+	    if ((age > = 18) && (age <= 35))
+                premium = 6.0;
+            else
+           if (age >= 36)
+                premium = 5.0;
+            else
+                premium = 0.0;
+else
+		premium = 0.0;
+
+            if (age >= 50)
+                premium = premium * 0.15;
+            return premium;
+        }
+
     }
 }
