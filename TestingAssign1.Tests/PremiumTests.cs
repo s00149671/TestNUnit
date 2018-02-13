4b5ed4d294af567608bc2a5790d00c2c23e6476a
s00149671 <s00149671@mail.itsligo.ie>
2018-02-13 16:44:40 +0000

    g

---
+++ TestingAssign1.Tests/PremiumTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingAssign1.Tests
+{
+    [TestFixture]
+    class PremiumTests
+    {
+        [Test]
+        public void test1()
+        {
+            var test = new Premium();
+
+            Assert.AreEqual(5, test.CalcPremium(20, "female"));
+           
+
+    }
+}
---
+++ TestingAssign1/Premium.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingAssign1
+{
+    public class Premium
+    {
+        public float CalcPremium(int age, string gender)
+        {
+            float premium;
+
+            if (gender == "female")
+                if ((age >= 18) && (age <= 30))
+                    premium = 5.0;
+                else
+                    if (age >= 31)
+                    premium = 2.50;
+                else
+                    premium = 0.0;
+            else
+    if (gender == "male")
+                if ((age >= 18) && (age <= 35))
+                    premium = 6.0;
+                else
+                   if (age >= 36)
+                    premium = 5.0;
+                else
+                    premium = 0.0;
+            else
+                premium = 0.0;
+
+            if (age >= 50)
+                premium = premium * 0.15;
+            return premium;
+        }
+    }
+}
